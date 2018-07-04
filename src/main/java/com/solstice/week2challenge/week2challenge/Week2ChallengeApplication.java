8c73cc307fbfb612bdc1fa79fb5493e63e6d6c4b
Tyree Solomon-Phillips <tyreesolomon-phillips@Tyree-Solomon-Phillips-MacBook-Pro.local>
2018-07-04 10:29:40 -0500

    added project setup files and a gitignore

---
+++ src/main/java/com/solstice/week2challenge/week2challenge/Week2ChallengeApplication.java
@@ -0,0 +1,12 @@
+package com.solstice.week2challenge.week2challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week2ChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Week2ChallengeApplication.class, args);
+	}
+}
---
+++ src/test/java/com/solstice/week2challenge/week2challenge/Week2ChallengeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.solstice.week2challenge.week2challenge;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Week2ChallengeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
